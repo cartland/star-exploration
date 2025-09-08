649a501a13c980a235a0e0a50551e53e20bac2fd
cartland <c.cartland@gmail.com>
2025-09-07 22:16:03 -0700

    fix(build): Add missing import for Vector2 in AnimationLoop

--- website/src/animation/AnimationLoop.js
+++ website/src/animation/AnimationLoop.js
@@ -16,6 +16,8 @@
  *
  */
 
+import { V } from '../model/Vector2';
+
 // eslint-disable-next-line no-extend-native
 Number.prototype.map = function (in_min, in_max, out_min, out_max) {
   return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
